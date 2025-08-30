15be91354c5b83461628f00978d1a3fc92ee716b
nelnn <nelsonn.xyz@gmail.com>
2025-08-30 17:49:19 +0100

    remove non cli stuff

--- scrap/scrape_yt.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-from rich import print
-
-resp = requests.get("https://noembed.com/embed?url=https://www.youtube.com/watch?v=ipRvjS7q1DI&t=199").json()
-print(resp)
--- src/api/router.py
+++
@@ -1,9 +0,0 @@
-"""Configure API routing."""
-
-import fastapi
-
-from src.api.endpoints import get_transcript
-from src.config import settings
-
-router = fastapi.APIRouter(responses={404: {"description": "Not found"}})
-router.include_router(get_transcript.router)
